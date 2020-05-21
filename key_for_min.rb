bcd70d5d65c0425a6ddeb6af6ae76f70c077bac2
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-21 02:42:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_val = 
+  name_hash.each { |key, val|
+    if val < min_val
+      min_key = key
+    end
+  }
+  min_key
 end
